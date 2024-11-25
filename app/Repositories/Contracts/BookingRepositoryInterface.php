c1da763b9967e0a581c8017da915ec2edc44b741
ValentaSaputra <valentaofficial@gmail.com>
2024-11-25 21:33:32 +0700

    add service repository pattern

---
+++ app/Repositories/Contracts/BookingRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?
+
+namespace App\Repositories\Contracts;
+
+interface BookingRepositoryInterface
+{
+    public function createBooking(array $data);
+    public function findByTrxIdAndPhoneNumber($bookingTrxId, $phoneNumber); 
+}
